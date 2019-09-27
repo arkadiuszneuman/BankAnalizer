802561e1c6d9cff2aa652359443ecab5150de79a
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-09-27 23:00:38 +0200

    Change project name to web. Add simple controller.

---
+++ backend/PkoAnalizer/PkoAnalizer/Controllers/ImportController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PkoAnalizer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<int> ImportAll()
+        {
+            return new[] { 1, 2, 4 };
+        }
+    }
+}
