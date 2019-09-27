086032b178ac16edb580d46a5f36e73dcbbf7392
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2019-09-27 23:52:44 +0200

    Register CQRS by autofac

---
+++ backend/PkoAnalizer/PkoAnalizer/Modules/Cqrs/CommandsModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using PkoAnalizer.Core.Cqrs.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PkoAnalizer.Web.Modules.Cqrs
+{
+    public class CommandsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(x => x.IsAssignableTo<IHandleCommand>())
+                .AsImplementedInterfaces();
+
+            builder.Register<Func<Type, IHandleCommand>>(c =>
+            {
+                var ctx = c.Resolve<IComponentContext>();
+
+                return t =>
+                {
+                    var handlerType = typeof(IHandleCommand<>).MakeGenericType(t);
+                    return (IHandleCommand)ctx.Resolve(handlerType);
+                };
+            });
+
+            builder.RegisterType<CommandsBus>()
+                .AsImplementedInterfaces();
+        }
+    }
+}
--- backend/PkoAnalizer/PkoAnalizer/Startup.cs
+++ backend/PkoAnalizer/PkoAnalizer/Startup.cs
@@ -1,36 +1,47 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace PkoAnalizer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+
+            this.Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
+
+        public ILifetimeScope AutofacContainer { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOptions();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyModules(this.GetType().Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
