d8364d9005dace45ade53bb67ea4a6fc217131f5
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-09-30 19:29:27 +0200

    Add transfer to phone outgoing importer

---
+++ backend/PkoAnalizer/PkoAnalizer.Logic/Import/Importers/TypeImporters/TransferToPhoneOutgoingImporter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PkoAnalizer.Logic.Import.Models;
+
+namespace PkoAnalizer.Logic.Import.Importers.TypeImporters
+{
+    public class TransferToPhoneOutgoingImporter : ITypeImporter
+    {
+        public PkoTransaction Import(string[] splittedLine)
+        {
+            var type = splittedLine.Index(2);
+            if (type == "Przelew na telefon wychodzący zew.")
+            {
+                return new PkoTransaction
+                {
+                    OperationDate = splittedLine.Index(0).ConvertToDate(),
+                    TransactionDate = splittedLine.Index(1).ConvertToDate(),
+                    TransactionType = splittedLine.Index(2),
+                    Amount = splittedLine.Index(3).ConvertToDecimal(),
+                    Currency = splittedLine.Index(4),
+                    RecipientReceipt = splittedLine.Index(6),
+                    RecipientName = splittedLine.Index(7),
+                    Title = splittedLine.Index(8)
+                };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ backend/PkoAnalizer/PkoAnalizer.Tests/Logic/Importers/TypeImporters/TransferToPhoneOutgoingImporterTests.cs
@@ -0,0 +1,54 @@
+﻿using AutofacContrib.NSubstitute;
+using FluentAssertions;
+using PkoAnalizer.Logic.Import.Importers.TypeImporters;
+using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PkoAnalizer.Tests.Logic.Importers.TypeImporters
+{
+    public class TransferToPhoneOutgoingImporterTests
+    {
+        [Fact]
+        public void Should_import_valid_card_transactions()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<TransferToPhoneOutgoingImporter>();
+
+            //act
+            var result = sut.Import(new[] {"2019-03-25","2019-03-26","Przelew na telefon wychodzący zew.",
+                "-32.12","PLN","+321.87","Rachunek odbiorcy: 44 5555 7777 1111 3333 6666 7777",
+                "Nazwa odbiorcy: NAME SURNAME","Tytuł: PRZELEW NA TELEFON OD: 48930293023 DO: 48950495023","","","",""
+            });
+
+            result.Should().BeEquivalentTo(new PkoTransaction
+            {
+                OperationDate = new DateTime(2019, 3, 25),
+                TransactionDate = new DateTime(2019, 3, 26),
+                TransactionType = "Przelew na telefon wychodzący zew.",
+                Amount = -32.12M,
+                Currency = "PLN",
+                RecipientReceipt = "Rachunek odbiorcy: 44 5555 7777 1111 3333 6666 7777",
+                RecipientName = "Nazwa odbiorcy: NAME SURNAME",
+                Title = "Tytuł: PRZELEW NA TELEFON OD: 48930293023 DO: 48950495023",
+            });
+        }
+
+        [Fact]
+        public void Should_not_import_different_type()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<TransferToPhoneOutgoingImporter>();
+
+            //act
+            var result = sut.Import(new[] {"2019-03-25","2019-03-26","Other type",
+                "-32.12","PLN","+321.87","Rachunek odbiorcy: 44 5555 7777 1111 3333 6666 7777",
+                "Nazwa odbiorcy: NAME SURNAME","Tytuł: PRZELEW NA TELEFON OD: 48930293023 DO: 48950495023","","","",""
+            });
+
+            result.Should().BeNull();
+        }
+    }
+}
