210ccd09a99145c345efc664bdddeb72749d7393
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-10-01 17:46:48 +0200

    Add withdrawal from atm

---
+++ backend/PkoAnalizer/PkoAnalizer.Tests/Logic/Importers/TypeImporters/ImportersFixture.cs
@@ -0,0 +1,41 @@
+﻿using PkoAnalizer.Logic.Import.Importers.TypeImporters;
+using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PkoAnalizer.Tests.Logic.Importers.TypeImporters
+{
+    public class ImportersFixture
+    {
+        public IEnumerable<(Type, string[], PkoTransaction)> GetTransactions()
+        {
+            yield return
+                (typeof(WithdrawalFromAtm),
+                new[] {
+                    "2019-02-09", "2019-02-08", "Wypłata z bankomatu", "-321.32", "PLN", "+32.73",
+                    "Tytuł: PKO BP 123123123", "Lokalizacja: Kraj: POLSKA Miasto: SOMECITY Adres: UL. SOMNEADDRESS 45",
+                    "Data i czas operacji: 2019-02-08 11:34:51", "Oryginalna kwota operacji: 32,73 PLN", "Numer karty: 425125******4284", "", ""
+                },
+                new PkoTransaction
+                {
+                    OperationDate = new DateTime(2019, 2, 9),
+                    TransactionDate = new DateTime(2019, 2, 8),
+                    TransactionType = "Wypłata z bankomatu",
+                    Amount = -321.32M,
+                    Currency = "PLN",
+                    Title = "Tytuł: PKO BP 123123123",
+                    Location = "Lokalizacja: Kraj: POLSKA Miasto: SOMECITY Adres: UL. SOMNEADDRESS 45",
+                });
+        }
+
+        public (string[], PkoTransaction) GetTransaction<T>()
+        {
+            return GetTransactions()
+                .Where(t => t.Item1 == typeof(T))
+                .Select(x => (x.Item2, x.Item3))
+                .Single();
+        }
+    }
+}
---
+++ backend/PkoAnalizer/PkoAnalizer.Tests/Logic/Importers/TypeImporters/WithdrawalFromAtmTests.cs
@@ -0,0 +1,42 @@
+﻿using AutofacContrib.NSubstitute;
+using FluentAssertions;
+using PkoAnalizer.Logic.Import.Importers.TypeImporters;
+using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PkoAnalizer.Tests.Logic.Importers.TypeImporters
+{
+    public class WithdrawalFromAtmTests
+    {
+        [Fact]
+        public void Should_import_withdrawal_from_atm_transactions()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<WithdrawalFromAtm>();
+            (var splittedLine, var transaction) = new ImportersFixture().GetTransaction<WithdrawalFromAtm>();
+
+            //act
+            var result = sut.Import(splittedLine);
+
+            //assert
+            result.Should().BeEquivalentTo(transaction);
+        }
+
+        [Fact]
+        public void Should_not_import_different_type()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<WebPaymentImporter>();
+
+            //act
+            var result = sut.Import(new[] { "2019-04-18","2019-04-17","Some other transaction",
+                "-10.19","PLN","+507.94","Numer telefonu: +48 568 457 587 ","Lokalizacja: Adres: www.skycash.com",
+                "Data i czas operacji: 2019-04-17 17:16:10","Numer referencyjny: 5458568544654","","","" });
+
+            result.Should().BeNull();
+        }
+    }
+}
