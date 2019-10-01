ecd42de000e5ee98f4297a24c9ac2efdf0b26a60
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-10-02 00:30:53 +0200

    Add cash payment in atc importer

---
+++ backend/PkoAnalizer/PkoAnalizer.Logic/Import/Importers/TypeImporters/CashPaymentInAtmImporter.cs
@@ -0,0 +1,30 @@
+﻿using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PkoAnalizer.Logic.Import.Importers.TypeImporters
+{
+    public class CashPaymentInAtmImporter : ITypeImporter
+    {
+        public PkoTransaction Import(string[] splittedLine)
+        {
+            var type = splittedLine.Index(2);
+            if (type == "Wpłata gotówki we wpłatomacie")
+            {
+                return new PkoTransaction
+                {
+                    OperationDate = splittedLine.Index(0).ConvertToDate(),
+                    TransactionDate = splittedLine.Index(1).ConvertToDate(),
+                    TransactionType = splittedLine.Index(2),
+                    Amount = splittedLine.Index(3).ConvertToDecimal(),
+                    Currency = splittedLine.Index(4),
+                    Title = splittedLine.Index(6),
+                    Location = splittedLine.Index(7),
+                };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ backend/PkoAnalizer/PkoAnalizer.Tests/Logic/Importers/TypeImporters/CashPaymentInAtmImporterTests.cs
@@ -0,0 +1,60 @@
+﻿using AutofacContrib.NSubstitute;
+using FluentAssertions;
+using PkoAnalizer.Logic.Import.Importers.TypeImporters;
+using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PkoAnalizer.Tests.Logic.Importers.TypeImporters
+{
+    public class CashPaymentInAtmTests
+    {
+        [Fact]
+        public void Should_import_cash_payment_in_atc_transactions()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<CashPaymentInAtm>();
+
+            //act
+            var result = sut.Import(new[] {
+                "2018-03-23","2018-03-24","Wpłata gotówki we wpłatomacie","+11.21","PLN","+32.22",
+                "Tytuł: PKO BP 123123123123",
+                "Lokalizacja: Kraj: COUNTRY Miasto: CITY Adres: STREET",
+                "Data i czas operacji: 2018-03-23 17:07:35","Oryginalna kwota operacji: 11,21 PLN",
+                "Numer karty: 1234*****323","",""
+            });
+
+            //assert
+            result.Should().BeEquivalentTo(new PkoTransaction
+            {
+                OperationDate = new DateTime(2018, 3, 23),
+                TransactionDate = new DateTime(2018, 3, 24),
+                TransactionType = "Wpłata gotówki we wpłatomacie",
+                Amount = 11.21M,
+                Currency = "PLN",
+                Title = "Tytuł: PKO BP 123123123123",
+                Location = "Lokalizacja: Kraj: COUNTRY Miasto: CITY Adres: STREET"
+            });
+        }
+
+        [Fact]
+        public void Should_not_import_different_type()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<WebPaymentImporter>();
+
+            //act
+            var result = sut.Import(new[] { "2018-03-23","2018-03-24","Other type","+11.21","PLN","+32.22",
+                "Tytuł: PKO BP 123123123123",
+                "Lokalizacja: Kraj: COUNTRY Miasto: CITY Adres: STREET",
+                "Data i czas operacji: 2018-03-23 17:07:35","Oryginalna kwota operacji: 11,21 PLN",
+                "Numer karty: 1234*****323","",""
+            });
+
+            //assert
+            result.Should().BeNull();
+        }
+    }
+}
