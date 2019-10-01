2f51a7a9c6ce13246675e96dedf04f15f8f30cf3
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-10-02 00:14:04 +0200

    Add direct debit importer

---
+++ backend/PkoAnalizer/PkoAnalizer.Logic/Import/Importers/TypeImporters/DirectDebitTypeImporter.cs
@@ -0,0 +1,32 @@
+﻿using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PkoAnalizer.Logic.Import.Importers.TypeImporters
+{
+    public class DirectDebitTypeImporter : ITypeImporter
+    {
+        public PkoTransaction Import(string[] splittedLine)
+        {
+            var type = splittedLine.Index(2);
+            if (type == "Polecenie Zapłaty")
+            {
+                return new PkoTransaction
+                {
+                    OperationDate = splittedLine.Index(0).ConvertToDate(),
+                    TransactionDate = splittedLine.Index(1).ConvertToDate(),
+                    TransactionType = splittedLine.Index(2),
+                    Amount = splittedLine.Index(3).ConvertToDecimal(),
+                    Currency = splittedLine.Index(4),
+                    RecipientReceipt = splittedLine.Index(6),
+                    RecipientName = splittedLine.Index(7),
+                    RecipientAddress = splittedLine.Index(8),
+                    Title = splittedLine.Index(9),
+                };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ backend/PkoAnalizer/PkoAnalizer.Tests/Logic/Importers/TypeImporters/DirectDebitTypeImporterTests.cs
@@ -0,0 +1,60 @@
+﻿using AutofacContrib.NSubstitute;
+using FluentAssertions;
+using PkoAnalizer.Logic.Import.Importers.TypeImporters;
+using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PkoAnalizer.Tests.Logic.Importers.TypeImporters
+{
+    public class DirectDebitTypeImporterTests
+    {
+        [Fact]
+        public void Should_import_direct_debit_transactions()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<DirectDebitTypeImporter>();
+
+            //act
+            var result = sut.Import(new[] {
+                "2019-03-23","2019-03-24","Polecenie Zapłaty","-21.32","PLN","+21.48",
+                "Rachunek odbiorcy: 88 7777 2222 4444 0000 6666 9999","Nazwa odbiorcy: SOME RECIPIENT",
+                "Adres odbiorcy: SOME RECIPIENT ADDRESS",
+                "Tytuł: SOME TITLE","","",""
+            });
+
+            //assert
+            result.Should().BeEquivalentTo(new PkoTransaction
+            {
+                OperationDate = new DateTime(2019, 3, 23),
+                TransactionDate = new DateTime(2019, 3, 24),
+                TransactionType = "Polecenie Zapłaty",
+                Amount = -21.32M,
+                Currency = "PLN",
+                RecipientReceipt = "Rachunek odbiorcy: 88 7777 2222 4444 0000 6666 9999",
+                RecipientName = "Nazwa odbiorcy: SOME RECIPIENT",
+                RecipientAddress = "Adres odbiorcy: SOME RECIPIENT ADDRESS",
+                Title = "Tytuł: SOME TITLE",
+            });;
+        }
+
+        [Fact]
+        public void Should_not_import_different_type()
+        {
+            //arrange
+            var sut = new AutoSubstitute().Resolve<WebPaymentImporter>();
+
+            //act
+            var result = sut.Import(new[] { "2019-03-23","2019-03-24","Other type","-21.32","PLN","+21.48",
+                "Rachunek odbiorcy: 88 7777 2222 4444 0000 6666 9999","Nazwa odbiorcy: SOME RECIPIENT",
+                "Adres odbiorcy: SOME RECIPIENT ADDRESS",
+                "Tytuł: SOME TITLE","","",""
+            });
+
+            //assert
+            result.Should().BeNull();
+        }
+    }
+}
