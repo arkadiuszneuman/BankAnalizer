58c9ef282ce1fb74775d112af11fec90966aaa36
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2019-10-02 00:31:08 +0200

    Add return card payment importer tests

---
+++ backend/PkoAnalizer/PkoAnalizer.Logic/Import/Importers/TypeImporters/ReturnCardPaymentImporter.cs
@@ -0,0 +1,30 @@
+﻿using PkoAnalizer.Logic.Import.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PkoAnalizer.Logic.Import.Importers.TypeImporters
+{
+    public class ReturnCardPaymentImporter : ITypeImporter
+    {
+        public PkoTransaction Import(string[] splittedLine)
+        {
+            var type = splittedLine.Index(2);
+            if (type == "Zwrot płatności kartą")
+            {
+                return new PkoTransaction
+                {
+                    OperationDate = splittedLine.Index(0).ConvertToDate(),
+                    TransactionDate = splittedLine.Index(1).ConvertToDate(),
+                    TransactionType = splittedLine.Index(2),
+                    Amount = splittedLine.Index(3).ConvertToDecimal(),
+                    Currency = splittedLine.Index(4),
+                    Title = splittedLine.Index(6),
+                    RecipientAddress = splittedLine.Index(7)
+                };
+            }
+
+            return null;
+        }
+    }
+}
