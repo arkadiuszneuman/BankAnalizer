097e9b16e05c56bac4a867a32308decac2c8dc22
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2020-03-09 23:11:28 +0100

    Add to menu login and register

--- frontend/pkoanalizer/src/components/Menu/index.js
+++ frontend/pkoanalizer/src/components/Menu/index.js
@@ -13,6 +13,8 @@ class Menu extends Component {
                   <NavLink to="/" exact={true} activeClassName="active" className="item">Main</NavLink>
                   <NavLink to="/rules" activeClassName="active" className="item">Rules</NavLink>
                   <NavLink to="/charts" activeClassName="active" className="item">Charts</NavLink>
+                  <NavLink to="/register" activeClassName="active" className="item">Register</NavLink>
+                  <NavLink to="/login" activeClassName="active" className="item">Login</NavLink>
               </div>
           </div>
     )
