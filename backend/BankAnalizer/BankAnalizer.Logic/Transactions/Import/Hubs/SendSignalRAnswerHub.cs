45f52921d78f870f60d1a9c51bc4e294402dcdb6
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2020-04-09 22:08:47 +0200

    Raname frontend to bankanalizer

--- backend/BankAnalizer/BankAnalizer.Logic/Transactions/Import/Hubs/SendSignalRAnswerHub.cs
+++ backend/BankAnalizer/BankAnalizer.Logic/Transactions/Import/Hubs/SendSignalRAnswerHub.cs
@@ -1,24 +1,22 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace BankAnalizer.Logic.Transactions.Import.Hubs
 {
     public class SendSignalRAnswerHub : Hub
     {
-        private static readonly Dictionary<Guid, List<string>> registeredClients = new Dictionary<Guid, List<string>>();
+        private static readonly ConcurrentDictionary<Guid, List<string>> registeredClients = new ConcurrentDictionary<Guid, List<string>>();
 
-        public string GetConnectionId()
-        {
-            return Context.ConnectionId;
-        }
+        public string GetConnectionId() => Context.ConnectionId;
 
         public void RegisterClient(Guid clientId, string connectionId)
         {
             if (registeredClients.ContainsKey(clientId))
                 registeredClients[clientId].Add(connectionId);
             else
-                registeredClients.Add(clientId, new List<string> { connectionId });
+                registeredClients.TryAdd(clientId, new List<string> { connectionId });
         }
 
         public static List<string> GetRegisteredClients(Guid clientId)
