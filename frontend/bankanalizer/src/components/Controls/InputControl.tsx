680508819e4f040ef1e58cd7805663552275ab95
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2020-08-29 18:45:15 +0200

    Add simple input reacting to enter

--- frontend/bankanalizer/src/components/Controls/InputControl.tsx
+++ frontend/bankanalizer/src/components/Controls/InputControl.tsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react"
 
+export interface IChange {
+    name: string,
+    value: string,
+    event: React.ChangeEvent
+}
+
 interface IProps {
     name: string,
-    text: string,
-    initValue?: string,
+    text?: string,
+    value?: string,
     type?: string,
-    onChange?: (change: any) => void
+    setValue?: (value: string) => void,
+    onChange?: (change: IChange) => void,
+    onKeyDown?: (key: HTMLInputElement, event: React.KeyboardEvent) => void
+    onKeyPress?: (key: any) => void
 }
 
 interface IState {
@@ -15,14 +24,32 @@ interface IState {
 
 export default class InputControl extends Component<IProps, IState> {
     state = {
-        value: this.props.initValue ?? '',
+        value: this.props.value ?? '',
         type: this.props.type ?? 'text'
+    }
+
+    componentDidUpdate = (prevProps: Readonly<IProps>, prevState: Readonly<IState>) => {
+        if (prevProps.value && prevProps.value !== this.props.value && this.props.value !== this.state.value) {
+            this.setState({ value: this.props.value as string })
+        }
     }
 
     handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({ value: e.target.value });
         if (this.props.onChange) {
-            this.props.onChange({ name: this.props.name, value: e.target.value })
+            this.props.onChange({ name: this.props.name, value: e.target.value, event: e })
+        }
+    }
+
+    handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (this.props.onKeyDown) {
+            this.props.onKeyDown(e.target as HTMLInputElement, e)
+        }
+    }
+
+    onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (this.props.onKeyPress) {
+            this.props.onKeyPress(e)
         }
     }
 
@@ -35,7 +62,13 @@ export default class InputControl extends Component<IProps, IState> {
         return (
             <div className="field">
                 <label>{text}</label>
-                <input type={type} value={value} name={name} onChange={this.handleChange} placeholder={text} />
+                <input  type={type} 
+                        value={value}
+                        name={name}
+                        placeholder={text}
+                        onChange={this.handleChange}
+                        onKeyPress={this.onKeyPress}
+                        onKeyDown={this.handleKeyDown} />
             </div>
         )
     }
--- frontend/bankanalizer/src/components/UsersConnections/index.tsx
+++ frontend/bankanalizer/src/components/UsersConnections/index.tsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import {apiConnector} from '../../helpers/BankAnalizer'
 import ConnectionCard from './ConnectionCard'
+import Input, { IChange } from '../Controls/InputControl'
 
 interface IState {
     connections: any[]
+    value: string
 }
 
 export default class UsersConnections extends Component<{}, IState> {
 
     state = {
-        connections: []
-    }
+        connections: [],
+        value: ''
+    } as IState
 
     componentDidMount = async () => {
         await this.loadConnections()
@@ -20,12 +23,37 @@ export default class UsersConnections extends Component<{}, IState> {
         const connections = await apiConnector.get('usersconnection', { showAlsoAsRequestedUser: true })
         this.setState({ connections: connections })
     }
+
+    sendInvitation = (userToSendInvitation: string) => {
+        console.log(userToSendInvitation)
+    }
+
+    onKeyDown = (target: HTMLInputElement, event: React.KeyboardEvent) => {
+        const keyCode = event.keyCode || event.which;
+        if (keyCode === 13) {
+            event.preventDefault();
+            if (target.value) {
+                this.sendInvitation(target.value);
+                this.setState({ value: '' })
+            }
+            return false;
+        }
+    }
+
+    onChange = (change: IChange) => {
+        this.setState({ value: change.value })
+    }
    
     render() {
         const connections = this.state.connections
+        const value = this.state.value
 
         return (
             <div>
+                
+                <form className="ui form">
+                    <Input name="userToConnect" text="User to connect" value={value} onKeyDown={this.onKeyDown} onChange={this.onChange} />
+                </form>
                 <div className="ui cards">
                     {connections.map((connection: any) => 
                         <ConnectionCard key={connection.requestedUserId} connection={connection} />
